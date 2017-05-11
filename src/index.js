f94252592bff4da525bd607b0e210520a78ed7ec
jzs <jinzhongshuai@antiy.cn>
2017-05-11 17:42:35 +0800

    layout完成

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import version from './version';
-
+// components
 import Button from './components/button';
 import Navpath from './components/navpath';
 import Tooltip from './components/tooltip';
 import Icon from './components/icon';
+import Layout from './components/layout';
 
 export default {
   version,
   Button,
   Navpath,
   Tooltip,
-  Icon
+  Icon,
+  Layout
 };
