9e9447686441e9cc5314d19d8cbb7edf212faedd
guoyang <879515093@qq.com>
2017-05-15 09:05:51 +0800

    merge

--- example/index.js
+++ example/index.js
@@ -6,12 +6,9 @@ import DataTable from './pages/datatable';
 import Layout from './pages/layout';
 import Message from './pages/message';
 import SearchBar from './pages/searchbar';
-<<<<<<< HEAD
 import SliderContainer from './pages/slidercontainer';
 import Map from './pages/map';
 import Editor from './pages/editor';
-=======
->>>>>>> 29886f7e66d223be5d4766208fd8d8b3604108a5
 
 export default {
   Button,
@@ -22,10 +19,7 @@ export default {
   Layout,
   Message,
   SearchBar,
-<<<<<<< HEAD
   SliderContainer,
   Map,
   Editor,
-=======
->>>>>>> 29886f7e66d223be5d4766208fd8d8b3604108a5
 };
--- src/index.js
+++ src/index.js
@@ -7,13 +7,10 @@ import Icon from './components/icon';
 import DataTable from './components/datatable';
 import Layout from './components/layout';
 import Message from './components/message';
-<<<<<<< HEAD
 import SliderContainer from './components/slidercontainer';
 import Map from './components/map';
 import Editor from './components/editor';
-=======
 import Form from './components/form';
->>>>>>> 29886f7e66d223be5d4766208fd8d8b3604108a5
 
 export default {
   version,
@@ -24,11 +21,8 @@ export default {
   DataTable,
   Layout,
   Message,
-<<<<<<< HEAD
   SliderContainer,
   Map,
   Editor,
-=======
   Form,
->>>>>>> 29886f7e66d223be5d4766208fd8d8b3604108a5
 };
